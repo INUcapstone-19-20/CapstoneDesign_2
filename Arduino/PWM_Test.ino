6a82401832230251d18e119744e356b1789a8ab0
ByungSooSon <54934344+ByungSooSon@users.noreply.github.com>
2020-03-31 02:45:13 +0900

    Update PWM_Test.ino

--- Arduino/PWM_Test.ino
+++ Arduino/PWM_Test.ino
@@ -2,19 +2,15 @@
 #define NUM_LED_ROWS (4)
 #define NUM_BTN_COLUMNS (4)
 #define NUM_BTN_ROWS (4)
-#define NUM_COLORS (1)
 
 
-// Global variables
+// 전역변수
 static bool LED_buffer[NUM_LED_COLUMNS][NUM_LED_ROWS];
 
 static const uint8_t btncolumnpins[NUM_BTN_COLUMNS] = {29, 28, 27, 26};
 static const uint8_t btnrowpins[NUM_BTN_ROWS]       = {22, 23, 24, 25};
 static const uint8_t ledcolumnpins[NUM_LED_COLUMNS] = {30, 31, 32, 33};
-static const uint8_t colorpins[NUM_LED_ROWS][3]     = {{2, 3, 44}, {5, 6, 7}, {8, 9, 10}, {11, 12, 45}};
-
-static int16_t temp = 0;
-static bool br_up = true;
+static const uint8_t ledrowpins[NUM_LED_ROWS][3]     = {{2, 3, 44}, {5, 6, 7}, {8, 9, 10}, {11, 12, 45}};
 
 void setup()
 {
@@ -25,52 +21,28 @@ void setup()
     pinMode(ledcolumnpins[i],OUTPUT);
     pinMode(btncolumnpins[i],OUTPUT);
     pinMode(btnrowpins[i],OUTPUT);
-    for(uint8_t j;j<3;j++){
-      pinMode(colorpins[i][j],OUTPUT);
-    }
-    digitalWrite(ledcolumnpins[i], HIGH);
+    digitalWrite(ledcolumnpins[i], LOW);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("start");
   
-//  digitalWrite(btncolumnpins[0], LOW);
-  digitalWrite(ledcolumnpins[0], LOW);
-  digitalWrite(ledcolumnpins[1], LOW);
-  digitalWrite(ledcolumnpins[2], LOW);
-  digitalWrite(ledcolumnpins[3], LOW);
-//  digitalWrite(btnrowpins[0], LOW);
+  analogWrite(ledrowpins[0][0], 10);
+  analogWrite(ledrowpins[0][1], 10);
+  analogWrite(ledrowpins[0][2], 10);
+  
+  analogWrite(ledrowpins[1][0], 10);
+  analogWrite(ledrowpins[1][1], 10);
+  analogWrite(ledrowpins[1][2], 10);
+  
+  analogWrite(ledrowpins[2][0], 10);
+  analogWrite(ledrowpins[2][1], 10);
+  analogWrite(ledrowpins[2][2], 10);
+  
+  analogWrite(ledrowpins[3][0], 10);
+  analogWrite(ledrowpins[3][1], 10);
+  analogWrite(ledrowpins[3][2], 10);
 
-//  digitalWrite(colorpins[0],HIGH);
-//  digitalWrite(colorpins[1],HIGH);
-//  digitalWrite(colorpins[2],LOW);
-//  digitalWrite(colorpins[3],HIGH);
-//  for(uint8_t i=0;i<NUM_LED_COLUMNS;i++){
-////    analogWrite(colorpins[2], val);
-//  }
-  Serial.println("first");
-  for(uint8_t i=0;i<NUM_LED_COLUMNS;i++){
-    analogWrite(colorpins[i][0], 255-temp);
-    analogWrite(colorpins[i][1], temp);
-    
-  }
-  delay(10);
-  if(br_up){
-    temp += 1;
-    if(temp >= 255)
-      br_up = false;
-  }
-  else{
-    temp -= 1;
-    if(temp <= 0)
-      br_up = true;
-  }
-  
-  Serial.println(temp);
-//  analogWrite(colorpins[0], 10);
-//  analogWrite(colorpins[1], 90);
-//  analogWrite(colorpins[2], 150);
-//  analogWrite(colorpins[3], 255);
-  
+  Serial.println("end");
 }
