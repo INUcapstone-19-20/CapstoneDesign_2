88c7f5477bac420f2a6a994c05cf3974513855d6
sayheyBongmany <kbm0598@gmail.com>
2020-04-09 16:01:18 +0900

    func ScreenChange

--- GUI/mine_importUI.py
+++ GUI/mine_importUI.py
@@ -3,6 +3,7 @@ import PyQt5.QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtGui import QIcon
 from PyQt5 import uic
+from functools import partial
 
 
 #connect UI
@@ -12,53 +13,48 @@ screen4 = uic.loadUiType("ui/single_setting.ui")[0]
 screen11 = uic.loadUiType("ui/battlemode.ui")[0]
 
 
+# Screen change
+def ChangeScreen(before, screen_number):
+    if(screen_number == 2): before.main = ModeSelect()
+    elif(screen_number == 3): before.main = SingleMode()
+    elif(screen_number == 4): before.main = Single_Setting()
+    elif(screen_number == 11): before.main = BattleMode()
+
+    before.main.showFullScreen()
+    before.close()
+
+        
 class ModeSelect(QMainWindow, screen2):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # Button function
-        self.btn_single.clicked.connect(self.click_single)
-        self.btn_battle.clicked.connect(self.click_battle)
+        # Button connect
+        self.btn_single.clicked.connect(partial(ChangeScreen, self, 3))
+        self.btn_battle.clicked.connect(partial(ChangeScreen, self, 11))
 
-    def click_single(self):
-        self.main = SingleMode()
-        self.main.showFullScreen()
-        self.close()
-
-    def click_battle(self):
-        self.main = BattleMode()
-        self.main.showFullScreen()
-        self.close()
 
 class SingleMode(QMainWindow, screen3):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        
+        self.btn_singlesetting.clicked.connect(partial(ChangeScreen, self, 4))
 
-        self.btn_singlesetting.clicked.connect(self.click_setting)
-
-    def click_setting(self):
-        self.main = Single_Setting()
-        self.main.showFullScreen()
-        self.close()
 
 class Single_Setting(QMainWindow, screen4):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
         # Button image
         self.btn_settingback.setStyleSheet('image:url(res/btn_back1.png); border:0px;')
         self.btn_countup.setStyleSheet('image:url(res/btn_up.png); border:0px;')
         self.btn_countdown.setStyleSheet('image:url(res/btn_down.png); border:0px;')
         self.btn_timerup.setStyleSheet('image:url(res/btn_up.png); border:0px;')
         self.btn_timerdown.setStyleSheet('image:url(res/btn_down.png); border:0px;')
+        
+        self.btn_settingback.clicked.connect(partial(ChangeScreen, self, 3))
 
-        self.btn_settingback.clicked.connect(self.click_back)
-
-    def click_back(self):
-        self.main = ModeSelect()
-        self.main.showFullScreen()
-        self.close()
 
 class BattleMode(QMainWindow, screen11):
     def __init__(self):
