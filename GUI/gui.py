bef902cd142419141124a3c133b5bfc5bcbc0a31
YeEunLee918 <55092588+YeEunLee918@users.noreply.github.com>
2020-04-22 23:05:03 +0900

    Update gui.py

--- GUI/gui.py
+++ GUI/gui.py
@@ -10,13 +10,14 @@ from functools import partial
 screen2 = uic.loadUiType("ui/modeselect.ui")[0]
 screen3 = uic.loadUiType("ui/singlemode.ui")[0]
 screen4 = uic.loadUiType("ui/single_setting.ui")[0]
-screen10 = uic.loadUIType("ui/Replay_Game.ui")[0]
+screen10 = uic.loadUiType("ui/Replay_Game.ui")[0]
 screen11 = uic.loadUiType("ui/battlemode.ui")[0]
-screen15 = uic.loadUitype("ui/redturn.ui")[0]
-screen19 = uic.loadUitype("ui/blueturn.ui")[0]
-screen20 = uic.loadUitype("ui/Result.ui")[0]
-screen21 = uic.loadUitype("ui/single_default.ui")[0]
-
+screen15 = uic.loadUiType("ui/redturn.ui")[0]
+screen19 = uic.loadUiType("ui/blueturn.ui")[0]
+screen20 = uic.loadUiType("ui/Result.ui")[0]
+screen21 = uic.loadUiType("ui/single_default.ui")[0]
+screen23 = uic.loadUiType("ui/blue_loose.ui")[0]
+screen24 = uic.loadUiType("ui/red_loose.ui")[0]
 
 # Screen change
 def ChangeScreen(before, screen_number):
@@ -29,6 +30,8 @@ def ChangeScreen(before, screen_number):
     elif(screen_number == 19): before.main = Blueturn()
     elif(screen_number == 20): before.main = Result()
     elif(screen_number == 21): before.main = Single_Default()
+    elif(screen_number == 23): before.main = Blue_Loose()
+    elif(screen_number == 24): before.main = Red_Loose()
 
     before.main.showFullScreen()
     before.close()
@@ -49,7 +52,7 @@ class SingleMode(QMainWindow, screen3):
         self.setupUi(self)
         
         self.btn_singlesetting.clicked.connect(partial(ChangeScreen, self, 4))
-        self.btn_singlestart.clicked.coneect(partial(ChangeScreen, self, 21))
+        self.btn_singlestart.clicked.connect(partial(ChangeScreen, self, 21))
 
 
 class Single_Setting(QMainWindow, screen4):
@@ -87,35 +90,47 @@ class BattleMode(QMainWindow, screen11):
 
 
 class Redturn(QMainWindow, screen15):
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
         self.setupUi(self)
 
         self.btn_redturn.setStyleSheet('image:url(res/reddice_default.png); boarder:0px')
 
 
 class Blueturn(QMainWindow, screen19):
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
         self.setupUi(self)
 
         self.btn_blueturn.setStyleSheet('image:url(res/bluedice_default.png); boarder:0px')
 
 
 class Result(QMainWindow, screen20):
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
         self.setupUi(self)
 
         self.btn_restart.clicked.connect(partial(ChangeScreen, self, 11))
         self.btn_new.clicked.connect(partial(ChangeScreen, self, 2))
 
 class Single_Default(QMainWindow, screen21):
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
         self.setupUi(self)
 
+class Blue_Loose(QMainWindow, screen23):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
+        self.btn_bbom.clicked.connect(partial(ChangeScreen, self, 20))
+
+class Red_Loose(QMainWindow, screen24):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.btn_rbom.clicked.connect(partial(ChangeScreen, self, 20))
 
 
 if __name__ == '__main__':
