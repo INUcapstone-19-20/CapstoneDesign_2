915df7b7bf1577dc437ebcb8d1e50ab79f25e29e
ByungSooSon <sonbs95@naver.com>
2020-04-30 03:27:55 +0900

    test

--- Raspberry Pi/MineSweeper.py
+++ Raspberry Pi/MineSweeper.py
@@ -5,7 +5,6 @@ Turn = DiceResult                                                   # 턴 횟수
 KEYPAD_EDGE_RISING = 0                                              # 버튼 클릭
 numPixels = 16
 
-#KEYPAD = list(range(numPixels))                                    # 키패드         
 KEYPAD = [  0, 1, 2, 3,
             7, 6, 5, 4,
             8, 9, 10, 11,
