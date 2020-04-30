85c23db30f14cb78520d046f781682e0dd8ba8b4
ByungSooSon <sonbs95@naver.com>
2020-04-30 22:48:39 +0900

    패드 배열 순서 변경 및 주석 추가

--- Raspberry Pi/MineSweeper.py
+++ Raspberry Pi/MineSweeper.py
@@ -1,30 +1,47 @@
 import random
 import math
 
-DiceResult = 0                                                      # 주사위 결과치
-Turn = DiceResult                                                   # 턴 횟수
-KEYPAD_EDGE_RISING = 0                                              # 버튼 클릭
-numPixels = 16
-KEYPAD_ROW_NUM = 4                                                  # 키패드 행 갯수
-KEYPAD_COL_NUM = 4                                                  # 키패드 열 갯수
-red_turn = 1
-blue_turn = 0
+#DiceResult = 0                                           # 주사위 결과치
+#TurnNum = DiceResult                                     # 턴 횟수
+#KEYPAD_EDGE_RISING = 0                                   # 버튼 클릭
+#numPixels = 16
+KEYPAD_ROW_NUM = 4                                       # 키패드 행 갯수
+KEYPAD_COL_NUM = 4                                       # 키패드 열 갯수
+red_turn = 1                                             # 빨강 플레이어 턴                   
+blue_turn = 0                                            # 파랑 플레이어 턴  
 
 # 키패드                                   
 KEYPAD = [  [0, 1, 2, 3],                                           
-            [7, 6, 5, 4],
+            [4, 5, 6, 7],
             [8, 9, 10, 11],
-            [15, 14, 13, 12]]
+            [12, 13, 14, 15]]
 print('키패드 배열')
 for i in KEYPAD :
     for j in i:
-        print(j, end=' ')
+        print("%3d"%j, end=' ')
     print()
 print('')                                     
 
-# LED 색
+# 지뢰와의 거리에 따른 LED 색_거리 짧은 순
 Red = ['빨1','빨2','빨3']                   #[0xFFC6C6, 0xFF6363, 0xFF0000]
 Blue = ['파1','파2','파3']                  #[0xC6C6FF, 0x6363FF, 0x0000FF]
+
+# 플레이어 별 패드색 초기 선언
+RKEYPAD_COLOR = [['0' for col in range(KEYPAD_COL_NUM)] for row in range(KEYPAD_ROW_NUM)]       # 0 for ~
+BKEYPAD_COLOR = [['0' for col in range(KEYPAD_COL_NUM)] for row in range(KEYPAD_ROW_NUM)]       # 0 for ~
+
+print('빨간플레이어 패드색 선언')
+for i in RKEYPAD_COLOR :
+    for j in i:
+        print(j, end=' ')
+    print()
+print('')
+print('파랑플레이어 패드색 선언')
+for i in BKEYPAD_COLOR :
+    for j in i:
+        print(j, end=' ')
+    print()
+print('')
 
 # 지뢰 설정
 # 빨강플레이어의 지뢰
@@ -51,26 +68,10 @@ print('BMine :', BMine)
 print('')
 
 # 거리 알고리즘
-RKEYPAD_COLOR = [['0' for col in range(KEYPAD_COL_NUM)] for row in range(KEYPAD_ROW_NUM)]       # 0 for ~
-BKEYPAD_COLOR = [['0' for col in range(KEYPAD_COL_NUM)] for row in range(KEYPAD_ROW_NUM)]       # 0 for ~
-
-print('빨간플레이어 패드색 선언')
-for i in RKEYPAD_COLOR :
-    for j in i:
-        print(j, end=' ')
-    print()
-print('')
-print('파랑플레이어 패드색 선언')
-for i in BKEYPAD_COLOR :
-    for j in i:
-        print(j, end=' ')
-    print()
-print('')
-
 if red_turn :
     for r in range(0, KEYPAD_ROW_NUM):
         for c in range(0, KEYPAD_COL_NUM):
-            if ((r != RMine_ROW or c != RMine_COL) and (r != BMine_ROW or c != BMine_COL)):
+            if ((r != RMine_ROW or c != RMine_COL) and (r != BMine_ROW or c != BMine_COL)):         # 지뢰가 아닌 곳
                 distance = math.sqrt(math.pow((RMine_ROW - r), 2) + math.pow((RMine_COL - c), 2))
                 if 0 < distance < 2 :
                     RKEYPAD_COLOR[r][c] = Red[0]
@@ -78,13 +79,14 @@ if red_turn :
                     RKEYPAD_COLOR[r][c] = Red[1]
                 else :
                     RKEYPAD_COLOR[r][c] = Red[2]
-            elif (r == RMine_ROW and c == RMine_COL):
+            elif (r == RMine_ROW and c == RMine_COL):           # 빨강지뢰
                 RKEYPAD_COLOR[r][c] = '빨뢰'
-            elif (r == BMine_ROW and c == BMine_COL):
+            elif (r == BMine_ROW and c == BMine_COL):           # 파랑지뢰
                 RKEYPAD_COLOR[r][c] = '파뢰'
-elif blue_turn :                                # 위에서 red_turn = 0, blue_turn = 1 하면 구현됌
+elif blue_turn :                                                # 맨위에서 red_turn = 0, blue_turn = 1 하면 구현됌
     for r in range(0, KEYPAD_ROW_NUM):
         for c in range(0, KEYPAD_COL_NUM):
+            # 지뢰가 아닌 곳
             if ((r != RMine_ROW or c != RMine_COL) and (r != BMine_ROW or c != BMine_COL)):
                 distance = math.sqrt(math.pow((BMine_ROW - r), 2) + math.pow((BMine_COL - c), 2))
                 if 0 < distance < 2 :
@@ -93,6 +95,7 @@ elif blue_turn :                                # 위에서 red_turn = 0, blue_t
                     BKEYPAD_COLOR[r][c] = Blue[1]
                 else :
                     BKEYPAD_COLOR[r][c] = Blue[2]
+            # 지뢰인 곳
             elif (r == RMine_ROW and c == RMine_COL):
                 BKEYPAD_COLOR[r][c] = '빨뢰'
             elif (r == BMine_ROW and c == BMine_COL):
