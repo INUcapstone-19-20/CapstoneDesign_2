2ad36eadbc3320d5f9430d2f0fa3360b5a8b0747
suyeon00 <54935106+suyeon00@users.noreply.github.com>
2020-05-10 19:23:49 +0900

    Update buttonpad.ino

--- Arduino/buttonpad.ino
+++ Arduino/buttonpad.ino
@@ -2,6 +2,7 @@
 
 #define Y_DIM 4 // number of rows of keys
 #define X_DIM 4 // number of columns of keys
+#define COLORS 3  // number of colors
 
 // create a matrix of neotrellis boards
 Adafruit_NeoTrellis t_array[Y_DIM/4][X_DIM/4] = {
@@ -14,12 +15,12 @@ Adafruit_NeoTrellis t_array[Y_DIM/4][X_DIM/4] = {
 Adafruit_MultiTrellis trellis((Adafruit_NeoTrellis *)t_array, Y_DIM/4, X_DIM/4);
 
 // mine button color
-const static uint32_t mine_colors[3] = { 0xC5F255, 0x0FA453, 0x33AC25 };
+const static uint32_t mine_colors[4] = { 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF };
 
 // set players' color
-static uint32_t blue_colorcode[3] = { 0x0000FF, 0x6363FF, 0xC6C6FF };
-static uint32_t red_colorcode[3] = { 0xFF0000, 0xFF6363, 0xFFC6C6 };
-static uint32_t blue_colors[16], red_colors[16];
+static uint32_t blue_colorcode[COLORS] = { 0x0000FF, 0x5A5AFF, 0xB4B4FF };
+static uint32_t red_colorcode[COLORS] = { 0xFF0000, 0xFF5A5A, 0xFFB4B4 };
+static uint32_t blue_colors[Y_DIM*X_DIM], red_colors[Y_DIM*X_DIM];
 //static uint32_t blue_colors[16] = { 0x6363FF, 0x0000FF, 0x0000FF, 0x0000FF,     // - - - -
 //                                    0x6363FF, 0x0000FF, 0x000000, 0x0000FF,     // - - * -
 //                                    0x6363FF, 0x0000FF, 0x0000FF, 0x0000FF,     // - - - -
@@ -34,6 +35,8 @@ static uint8_t red_mine, blue_mine; // location of mine
 
 static uint8_t red_turn, blue_turn; // number of turns
 static boolean firstturn;
+
+static uint8_t ispressed[Y_DIM*X_DIM]; // button state. 1 is pressed, 0 is not pressed
 
 //  Input a value 0 to 255 to get a color value
 uint32_t Wheel(byte WheelPos) {
@@ -54,6 +57,33 @@ uint32_t Wheel(byte WheelPos) {
 
 double distance(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2) {
     return abs(sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)));
+}
+
+void setColor() {
+    uint8_t b_mine_x = blue_mine % Y_DIM;
+    uint8_t b_mine_y = blue_mine / Y_DIM;
+    for(uint8_t y=0; y<Y_DIM; y++) {
+      for(uint8_t x=0; x<X_DIM; x++) {
+        if(distance(b_mine_x, b_mine_y, x, y) <= sqrt(2)) 
+          blue_colors[y * Y_DIM + x] = blue_colorcode[0];
+        if(distance(b_mine_x, b_mine_y, x, y) > sqrt(2) && distance(b_mine_x, b_mine_y, x, y) < sqrt(8)) 
+          blue_colors[y * Y_DIM + x] = blue_colorcode[1];
+        if(distance(b_mine_x, b_mine_y, x, y) >= sqrt(8)) 
+          blue_colors[y * Y_DIM + x] = blue_colorcode[2];
+      }
+    }
+    uint8_t r_mine_x = red_mine % Y_DIM;
+    uint8_t r_mine_y = red_mine / Y_DIM;
+    for(uint8_t y=0; y<Y_DIM; y++) {
+      for(uint8_t x=0; x<X_DIM; x++) {
+        if(distance(r_mine_x, r_mine_y, x, y) <= sqrt(2)) 
+          red_colors[y * Y_DIM + x] = red_colorcode[0];
+        if(distance(r_mine_x, r_mine_y, x, y) > sqrt(2) && distance(r_mine_x, r_mine_y, x, y) < sqrt(8)) 
+          red_colors[y * Y_DIM + x] = red_colorcode[1];
+        if(distance(r_mine_x, r_mine_y, x, y) >= sqrt(8)) 
+          red_colors[y * Y_DIM + x] = red_colorcode[2];
+      }
+    }
 }
 
 // mine LED effect
@@ -145,7 +175,7 @@ void showMine(uint16_t mine_key, String color) {
         trellis.setPixelColor(mine_key, mine_colors[i]);
         trellis.show();
         delay(500);
-        if(i > 2) i = 0;
+        if(i > 4) i = 0;
         else i++;
         // 종료 조건 추가
     }
@@ -155,24 +185,28 @@ void showMine(uint16_t mine_key, String color) {
 TrellisCallback red_ON(keyEvent evt) {
     if(red_turn > 0) {
         if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
-            // pressed key is mine
-            if(evt.bit.NUM == red_mine) { // 빨간 플레이어 패배(본인 지뢰 클릭)
-                showMine(evt.bit.NUM, "red");
-                // 파이썬에 게임 종료 메시지 전송
-            } 
-            else if(evt.bit.NUM == blue_mine) { // 빨간 플레이어 승리
-                showMine(evt.bit.NUM, "blue");
-                // 파이썬에 게임 종료 메시지 전송
-            }
-            else {
-                trellis.setPixelColor(evt.bit.NUM, red_colors[evt.bit.NUM]);
-                trellis.show();
-                red_turn--;
-                if(red_turn == 0) {
-                    firstturn = !firstturn; // toggle turn state
-                    // loop 중단
-                    // 파이썬에 다음 턴에 대한 데이터 요청
+            if(ispressed[evt.bit.NUM] == 0) { // 눌리지 않은 버튼일 때
+                ispressed[evt.bit.NUM] = 1;
+                // 누른 버튼이 지뢰일 경우
+                if(evt.bit.NUM == red_mine) { // 빨간 플레이어 패배(본인 지뢰 클릭)
+                    showMine(evt.bit.NUM, "red");
+                    // 파이썬에 '게임 종료' 전송
+                } 
+                else if(evt.bit.NUM == blue_mine) { // 빨간 플레이어 승리
+                    showMine(evt.bit.NUM, "blue");
+                    // 파이썬에 '게임 종료' 전송
                 }
+                // 누른 버튼이 지뢰가 아닐 경우
+                else {
+                    trellis.setPixelColor(evt.bit.NUM, red_colors[evt.bit.NUM]);
+                    trellis.show();
+                    // 테스트용
+                    red_turn--;
+                    if(red_turn == 0) {
+                        firstturn = !firstturn; // toggle turn state
+                    }// end 테스트용
+                }                
+                // 파이썬에 '버튼 클릭 이벤트 발생' 전송
             }
         }
     }
@@ -183,24 +217,28 @@ TrellisCallback red_ON(keyEvent evt) {
 TrellisCallback blue_ON(keyEvent evt) {
     if(blue_turn > 0) {
         if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
-            // pressed key is mine
-            if(evt.bit.NUM == red_mine) { // 파란 플레이어 승리
-                showMine(evt.bit.NUM, "red");
-                // 파이썬에 게임 종료 메시지 전송
-            } 
-            else if(evt.bit.NUM == blue_mine) { // 파란 플레이어 패배(본인 지뢰 클릭)
-                showMine(evt.bit.NUM, "blue");
-                // 파이썬에 게임 종료 메시지 전송
-            }
-            else {
-                trellis.setPixelColor(evt.bit.NUM, blue_colors[evt.bit.NUM]);
-                trellis.show();
-                blue_turn--;
-                if(blue_turn == 0) {
-                    firstturn = !firstturn; // toggle turn state
-                    // loop 중단
-                    // 파이썬에 다음 턴에 대한 데이터 요청
+            if(ispressed[evt.bit.NUM] == 0) { // 눌리지 않은 버튼일 때
+                ispressed[evt.bit.NUM] = 1;
+                // 누른 버튼이 지뢰일 경우
+                if(evt.bit.NUM == red_mine) { // 파란 플레이어 승리
+                    showMine(evt.bit.NUM, "red");
+                    // 파이썬에 '게임 종료' 전송
+                } 
+                else if(evt.bit.NUM == blue_mine) { // 파란 플레이어 패배(본인 지뢰 클릭)
+                    showMine(evt.bit.NUM, "blue");
+                    // 파이썬에 '게임 종료' 전송
                 }
+                // 누른 버튼이 지뢰가 아닐 경우
+                else {
+                    trellis.setPixelColor(evt.bit.NUM, blue_colors[evt.bit.NUM]);
+                    trellis.show();
+                    // 테스트용
+                    blue_turn--;
+                    if(blue_turn == 0) {
+                        firstturn = !firstturn; // toggle turn state
+                    }// end 테스트용
+                }
+                // 파이썬에 '버튼 클릭 이벤트 발생' 전송
             }
         }
     }
@@ -222,33 +260,15 @@ void setup() {
     red_mine = 13;
 
     // set color array
-    uint8_t b_mine_x = blue_mine % Y_DIM;
-    uint8_t b_mine_y = blue_mine / Y_DIM;
-    for(uint8_t y=0; y<Y_DIM; y++) {
-      for(uint8_t x=0; x<X_DIM; x++) {
-        if(distance(b_mine_x, b_mine_y, x, y) <= sqrt(2)) 
-          blue_colors[y * Y_DIM + x] = blue_colorcode[0];
-        if(distance(b_mine_x, b_mine_y, x, y) > sqrt(2) && distance(b_mine_x, b_mine_y, x, y) <= sqrt(8)) 
-          blue_colors[y * Y_DIM + x] = blue_colorcode[1];
-        if(distance(b_mine_x, b_mine_y, x, y) > sqrt(8)) 
-          blue_colors[y * Y_DIM + x] = blue_colorcode[2];
-      }
-    }
-    uint8_t r_mine_x = red_mine % Y_DIM;
-    uint8_t r_mine_y = red_mine / Y_DIM;
-    for(uint8_t y=0; y<Y_DIM; y++) {
-      for(uint8_t x=0; x<X_DIM; x++) {
-        if(distance(r_mine_x, r_mine_y, x, y) <= sqrt(2)) 
-          red_colors[y * Y_DIM + x] = red_colorcode[0];
-        if(distance(r_mine_x, r_mine_y, x, y) > sqrt(2) && distance(r_mine_x, r_mine_y, x, y) <= sqrt(8)) 
-          red_colors[y * Y_DIM + x] = red_colorcode[1];
-        if(distance(r_mine_x, r_mine_y, x, y) > sqrt(8)) 
-          red_colors[y * Y_DIM + x] = red_colorcode[2];
-      }
-    }
+    setColor();
     
     // set turn state
     firstturn = true;
+
+    // initialize button state
+    for(uint8_t i=0; i<Y_DIM*X_DIM; i++) {
+      ispressed[i] = 0;
+    }
     
     // starting effect
     for(int i=0; i<Y_DIM*X_DIM; i++) {
@@ -275,12 +295,13 @@ uint8_t b_turns[6] = {1, 0, 1, 0, 1, 0};
 uint8_t it = 0;
 
 void loop() {
+    // 테스트용
     if(firstturn) { // 턴이 바뀌면 해당 플레이어의 턴 횟수 설정
         //red전달값 > blue전달값 ? red_turn = red전달값 : blue_turn = blue전달값;
         r_turns[it] > b_turns[it] ? red_turn = r_turns[it] : blue_turn = b_turns[it];
         firstturn = !firstturn; // toggle turn state
         it++;
-    }
+    }// end 테스트용
     
     // register a callback for all keys
     for(int i=0; i<Y_DIM*X_DIM; i++) {
