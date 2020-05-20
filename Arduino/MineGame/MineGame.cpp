30798118e8c595e46c30193e8c28b5f5f485d04a
suyeon00 <agew0423@daum.net>
2020-05-20 21:21:14 +0900

    Update 싱글모드, 배틀모드 클래스

--- Arduino/MineGame/MineGame.cpp
+++ Arduino/MineGame/MineGame.cpp
@@ -1,53 +1,55 @@
 #include "MineGame.h"
 
-SingleMode::SingleMode(uint16_t mine)
+SingleMode::SingleMode()
 {
-    this->_Y_DIM = _rows * 4; // _Y_DIM = number of rows of keys. _rows = number of rows of boards.
-    this->_X_DIM = _cols * 4; // _X_DIM = number of columns of keys. _cols = number of cols of boards.
-    
+    this->_trellis = (Adafruit_NeoTrellis *)Adafruit_MultiTrellis::_trelli;
+}
+
+void SingleMode::setGame(uint16_t mine)
+{
     // initialize button state
-    for(uint8_t i=0; i<_Y_DIM*_X_DIM; i++)
-        ispressed[i] = 0;
+    for(uint8_t i=0; i<ydim*xdim; i++)
+        this->ispressed[i] = 0;
 
-    setPlayer(mine);
+    // set player
+    this->player.ID = "single";
+    this->player.color[0] = SINGLE1;
+    this->player.color[1] = SINGLE2;
+    this->player.color[2] = SINGLE3;
+    this->player.mine = mine;
+
+    setColorArray(this->singlecolors);
 }
 
-void SingleMode::setPlayer(uint16_t mine) 
-{
-    player.ID = "single";
-    player.color1 = SINGLE1;
-    player.color2 = SINGLE2;
-    player.color3 = SINGLE3;
-    player.mine = mine;
-    setColorArray(player);
-}
-
-void SingleMode::setColorArray(Player p)
+void SingleMode::setColorArray(uint32_t *arr)
 {
     // number에서 x, y로 변경
-    uint8_t mine_x = p.mine % _X_DIM;
-    uint8_t mine_y = p.mine / _X_DIM;
-    for (uint8_t y = 0; y < _Y_DIM; y++) {
-        for (uint8_t x = 0; x < _X_DIM; x++) {
+    uint8_t mine_x = this->player.mine % xdim;
+    uint8_t mine_y = this->player.mine / xdim;
+    for (uint8_t y = 0; y < ydim; y++) {
+        for (uint8_t x = 0; x < xdim; x++) {
             double d = distance(mine_x, mine_y, x, y);
+            uint32_t keynum = y * xdim + x;
             if (d <= DISTANCE1) 
-                p.colors[y * _X_DIM + x] = p.color1;
+                arr[keynum] = SINGLE1;
             if (d > DISTANCE1 && DISTANCE2) 
-                p.colors[y * _X_DIM + x] = p.color2;
+                arr[keynum] = SINGLE2;
             if (d >= DISTANCE2) 
-                p.colors[y * _X_DIM + x] = p.color3;
+                arr[keynum] = SINGLE3;
         }
     }
 }
 
-void SingleMode::setCallback()
+void SingleMode::setCallback(void)
 {
-    for (int i = 0; i < _Y_DIM * _X_DIM; i++)
-        _trelli.registerCallback(i, led_ON);
+    for (int i = 0; i < ydim * xdim; i++)
+        this->_trellis.registerCallback(i, led_ON);
 }
 
-TrellisCallback SingleMode::led_ON(keyEvent evt)
+void SingleMode::(TrellisCallback *)led_ON(keyEvent evt)
 {
+    Adafruit_NeoTrellis *_t = this->_trellis;
+
     if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
         Serial.println("Click"); // 버튼 클릭시 Raspberrypi에 전달하기 위해 출력
         if (ispressed[evt.bit.NUM] == 0) {
@@ -55,8 +57,7 @@ TrellisCallback SingleMode::led_ON(keyEvent evt)
                 showMine(player);
             } else {
                 ispressed[evt.bit.NUM] = 1;
-                _trelli.setPixelColor(evt.bit.NUM, player.colors[evt.bit.NUM]);
-                _trelli.show();
+                _t.show(); 
             }
             // 파이썬에 '버튼 클릭 이벤트 발생' 전송
         }
@@ -64,50 +65,50 @@ TrellisCallback SingleMode::led_ON(keyEvent evt)
     return 0;
 }
 
-void SingleMode::startGame()
+void SingleMode::startGame(void)
 {
-    _trelli.read();
+    // '제한시간 안끝남 && 탐색횟수 남음' 일 때만 read
+    this->_trellis.read();
     delay(20);
 }
 
 void SingleMode::showMine(Player p)
 {
+    Adafruit_NeoTrellis *_t = this->_trellis;
+
     // number에서 x, y로 변경
-    uint8_t mine_x = p.mine % _X_DIM;
-    uint8_t mine_y = p.mine / _X_DIM;
+    uint8_t mine_x = p.mine % xdim;
+    uint8_t mine_y = p.mine / xdim;
     Serial.println("Boom");
 
     // 첫번째 영역 on
-    for (uint8_t y = 0; y < _Y_DIM; y++) {
-        for (uint8_t x = 0; x < _X_DIM; x++) {
+    for (uint8_t y = 0; y < ydim; y++) {
+        for (uint8_t x = 0; x < xdim; x++) {
             if (x == mine_x && y == mine_y) continue;
             if (distance(mine_x, mine_y, x, y) <= DISTANCE1) {
-                _trelli.setPixelColor(x, y, p.colors[y * _X_DIM + x]);
-                _trelli.show();
+                _t.show();
             }
         }
     }
     delay(500);
 
     // 두번째 영역 on
-    for (uint8_t y = 0; y < _Y_DIM; y++) {
-        for (uint8_t x = 0; x < _X_DIM; x++) {
+    for (uint8_t y = 0; y < ydim; y++) {
+        for (uint8_t x = 0; x < xdim; x++) {
             if (x == mine_x && y == mine_y) continue;
             if (distance(mine_x, mine_y, x, y) > DISTANCE1 && distance(mine_x, mine_y, x, y) < DISTANCE2) {
-                _trelli.setPixelColor(x, y, p.colors[y * _X_DIM + x]);
-                _trelli.show();
+                _t.show();
             }
         }
     }
     delay(500);
 
     // 세번째 영역 on
-    for (uint8_t y = 0; y < _Y_DIM; y++) {
-        for (uint8_t x = 0; x < _X_DIM; x++) {
+    for (uint8_t y = 0; y < ydim; y++) {
+        for (uint8_t x = 0; x < xdim; x++) {
             if (x == mine_x && y == mine_y) continue;
             if (distance(mine_x, mine_y, x, y) > DISTANCE2) {
-                _trelli.setPixelColor(x, y, p.colors[y * _X_DIM + x]);
-                _trelli.show();
+                _t.show();
             }
         }
     }
@@ -116,8 +117,8 @@ void SingleMode::showMine(Player p)
     // 지뢰
     uint8_t i = 0;
     while (1) {
-        _trelli.setPixelColor(p.mine, minecolor[i]);
-        _trelli.show();
+        _t.setPixelColor(p.mine, minecolor[i]);
+        _t.show();
         delay(500);
         if(i > 4) i = 0;
         else i++;
@@ -126,49 +127,69 @@ void SingleMode::showMine(Player p)
     }
 }
 
-BattleMode::BattleMode(uint16_t redmine, uint16_t bluemine)
+BattleMode::BattleMode()
 {
-    this->_Y_DIM = _rows * 4; // _Y_DIM = number of rows of keys. _rows = number of rows of boards.
-    this->_X_DIM = _cols * 4; // _X_DIM = number of columns of keys. _cols = number of cols of boards.
-    
+    this->_trellis = (Adafruit_NeoTrellis *)Adafruit_MultiTrellis::_trelli;
+}
+
+void BattleMode::setGame(uint16_t redmine, uint16_t bluemine)
+{
     // initialize button state
-    for(uint8_t i=0; i<_Y_DIM*_X_DIM; i++)
-        ispressed[i] = 0;
+    for(uint8_t i=0; i<ydim*xdim; i++)
+        this->ispressed[i] = 0;
 
-    setPlayer(redmine, bluemine);
+    // set Player1
+    this->playerRed.ID = "RED";
+    this->playerRed.color[0] = RED1;
+    this->playerRed.color[1] = RED2;
+    this->playerRed.color[2] = RED3;
+    this->playerRed.mine = redmine;
+
+    // setPlayer2
+    this->playerBlue.ID = "BLUE";
+    this->playerBlue.color[0] = BLUE1;
+    this->playerBlue.color[1] = BLUE2;
+    this->playerBlue.color[2] = BLUE3;
+    this->playerBlue.mine = bluemine;
+
+    setColorArray(this->playerRed, this->redcolors);
+    setColorArray(this->playerBlue, this->bluecolors);
 }
 
-void BattleMode::setPlayer(uint16_t redmine, uint16_t bluemine)
+void BattleMode::setColorArray(Player p, uint32_t *arr)
 {
-    // Player1
-    playerRed.ID = "RED";
-    playerRed.color1 = RED1;
-    playerRed.color2 = RED2;
-    playerRed.color3 = RED3;
-    playerRed.mine = redmine;
-    setColorArray(playerRed);
-
-    // Player2
-    playerBlue.ID = "BLUE";
-    playerBlue.color1 = BLUE1;
-    playerBlue.color2 = BLUE2;
-    playerBlue.color3 = BLUE3;
-    playerBlue.mine = bluemine;
-    setColorArray(playerBlue);
-}
-
-void BattleMode::setCallback(char turn)
-{
-    for (int i = 0; i < _Y_DIM * _X_DIM; i++) {
-        if (turn == 'R')
-            _trelli.registerCallback(i, red_ON);
-        else if (turn == 'B')
-            _trelli.registerCallback(i, blue_ON);
+    // number에서 x, y로 변경
+    uint8_t mine_x = p.mine % xdim;
+    uint8_t mine_y = p.mine / xdim;
+    for (uint8_t y = 0; y < ydim; y++) {
+        for (uint8_t x = 0; x < xdim; x++) {
+            double d = distance(mine_x, mine_y, x, y);
+            uint8_t keynum = y * xdim + x;
+            if (d <= DISTANCE1) 
+                arr[keynum] = p.color[0];
+            if (d > DISTANCE1 && DISTANCE2) 
+                arr[keynum] = p.color[1];
+            if (d >= DISTANCE2) 
+                arr[keynum] = p.color[2];
+            
+        }
     }
 }
 
-TrellisCallback BattleMode::red_ON(keyEvent evt)
+void setCallback(char turn)
 {
+    for (int i = 0; i < ydim * xdim; i++) {
+        if (turn == 'R')
+            this->_trellis.registerCallback(i, red_ON);
+        else if (turn == 'B')
+            this->_trellis.registerCallback(i, blue_ON);
+    }
+}
+
+void BattleMode::(TrellisCallback *)red_ON(keyEvent evt)
+{
+    Adafruit_NeoTrellis *_t = this->_trellis;
+
     if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
         Serial.println("Click"); // 버튼 클릭시 Raspberrypi에 전달하기 위해 출력
         if (ispressed[evt.bit.NUM] == 0) {
@@ -180,8 +201,8 @@ TrellisCallback BattleMode::red_ON(keyEvent evt)
                 // 파이썬에 'red 승리' 전송
             } else {
                 ispressed[evt.bit.NUM] = 1;
-                _trelli.setPixelColor(evt.bit.NUM, playerRed.colors[evt.bit.NUM]);
-                _trelli.show();
+                _t.setPixelColor(evt.bit.NUM, redcolors[evt.bit.NUM]);
+                _t.show();
             }
             // 파이썬에 '버튼 클릭 이벤트 발생' 전송
         }
@@ -189,8 +210,10 @@ TrellisCallback BattleMode::red_ON(keyEvent evt)
     return 0;
 }
 
-TrellisCallback blue_ON(keyEvent evt)
+void BattleMode::(TrellisCallback *)blue_ON(keyEvent evt)
 {
+    Adafruit_NeoTrellis *_t = this->_trellis;
+
     if (evt.bit.EDGE == SEESAW_KEYPAD_EDGE_RISING) {
         Serial.println("Click"); // 버튼 클릭시 Raspberrypi에 전달하기 위해 출력
         if (ispressed[evt.bit.NUM] == 0) {
@@ -202,11 +225,75 @@ TrellisCallback blue_ON(keyEvent evt)
                 // 파이썬에 'blue 승리' 전송
             } else {
                 ispressed[evt.bit.NUM] = 1;
-                _trelli.setPixelColor(evt.bit.NUM, playerBlue.colors[evt.bit.NUM]);
-                _trelli.show();
+                _t.setPixelColor(evt.bit.NUM, bluecolors[evt.bit.NUM]);
+                _t.show();
             }
             // 파이썬에 '버튼 클릭 이벤트 발생' 전송
         }
     }
     return 0;
+}
+
+void BattleMode::startGame(void)
+{
+    this->_trellis.read();
+    delay(20);
+}
+
+void BattleMode::showMine(Player p, uint32_t *arr)
+{
+    Adafruit_NeoTrellis *_t = this->_trellis;
+
+    // number에서 x, y로 변경
+    uint8_t mine_x = p.mine % xdim;
+    uint8_t mine_y = p.mine / xdim;
+    Serial.println("Boom");
+
+    // 첫번째 영역 on
+    for (uint8_t y = 0; y < ydim; y++) {
+        for (uint8_t x = 0; x < xdim; x++) {
+            if (x == mine_x && y == mine_y) continue;
+            if (distance(mine_x, mine_y, x, y) <= DISTANCE1) {
+                _t.setPixelColor(x, y, arr[y * xdim + x]);
+                _t.show();
+            }
+        }
+    }
+    delay(500);
+
+    // 두번째 영역 on
+    for (uint8_t y = 0; y < ydim; y++) {
+        for (uint8_t x = 0; x < xdim; x++) {
+            if (x == mine_x && y == mine_y) continue;
+            if (distance(mine_x, mine_y, x, y) > DISTANCE1 && distance(mine_x, mine_y, x, y) < DISTANCE2) {
+                _t.setPixelColor(x, y, arr[y * xdim + x]);
+                _t.show();
+            }
+        }
+    }
+    delay(500);
+
+    // 세번째 영역 on
+    for (uint8_t y = 0; y < ydim; y++) {
+        for (uint8_t x = 0; x < xdim; x++) {
+            if (x == mine_x && y == mine_y) continue;
+            if (distance(mine_x, mine_y, x, y) > DISTANCE2) {
+                _t.setPixelColor(x, y, arr[y * xdim + x]);
+                _t.show();
+            }
+        }
+    }
+    delay(500);
+
+    // 지뢰
+    uint8_t i = 0;
+    while (1) {
+        _t.setPixelColor(p.mine, minecolor[i]);
+        _t.show();
+        delay(500);
+        if(i > 4) i = 0;
+        else i++;
+        // 종료 조건 추가
+        // 파이썬에 '게임 종료' 전송
+    }
 }
--- Arduino/MineGame/MineGame.h
+++ Arduino/MineGame/MineGame.h
@@ -1,75 +1,80 @@
 #ifndef MINEGAME_H
 #define MINEGAME_H
 
-#include "Adafruit_NeoTrellis.h"
+#define ydim 4
+#define xdim 4
 
 #define COLORLEVEL 3
 
-enum COLOR {
-    SINGLE1 = 0x00FFFF,
-    SINGLE2 = 0x8aFFFF,
-    SINGLE3 = 0xe0FFFF,
-    RED1 = 0xFF0000,
-    RED2 = 0xFF5A5A,
-    RED3 = 0xFFB4B4,
-    BLUE1 = 0x0000FF,
-    BLUE2 = 0x5A5AFF, 
-    BLUE3 = 0xB4B4FF
-};
+#define SINGLE1 0x00FFFF
+#define SINGLE2 0x8aFFFF
+#define SINGLE3 0xe0FFFF
+#define RED1 0xFF0000
+#define RED2 0xFF5A5A
+#define RED3 0xFFB4B4
+#define BLUE1 0x0000FF
+#define BLUE2 0x5A5AFF 
+#define BLUE3 0xB4B4FF
 
-enum DISTANCE {
-    DISTANCE1 = sqrt(2),
-    DISTANCE2 = sqrt(8)
-};
+#define DISTANCE1 sqrt(2)
+#define DISTANCE2 sqrt(8)
 
 typedef struct Player {
     String ID;
-    COLOR color1, color2, color3;
+    uint32_t color[COLORLEVEL];
     uint16_t mine;
-    uint32_t colors[(NEO_TRELLIS_NUM_ROWS * _rows) * (NEO_TRELLIS_NUM_ROWS * _cols)];
 } Player;
 
 double distance(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2) {
     return abs(sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)));
 }
-
-class SingleMode : public Adafruit_MultiTrellis {
-public:
-    SingleMode(uint16_t mine);
+ 
+class SingleMode {
+public: 
+    SingleMode();
     ~SingleMode() {};
 
-    void setPlayer(uint16_t mine);
-    void setColorArray(Player p);
+    void setGame(uint16_t mine);
+    void setColorArray(uint32_t *arr);
 
-    void setCallback();
-    TrellisCallback led_ON(keyEvent evt);
+    void setCallback(void);
+    void (TrellisCallback *)led_ON(keyEvent evt);
 
-    void startGame();
+    void startGame(void);
 
     void showMine(Player p);
 
     friend class BattleMode;
 
-protected:
-    uint8_t _Y_DIM, _X_DIM;
-    Player player;
-    uint8_t ispressed[(NEO_TRELLIS_NUM_ROWS * _rows) * (NEO_TRELLIS_NUM_ROWS * _cols)]; // button state. 1 is pressed, 0 is not pressed
-    uint32_t minecolor[4] = { 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF };
-}
-
-class BattleMode {
 public:
-    BattleMode(uint16_t redmine, uint16_t bluemine);
+    Adafruit_MultiTrellis *_trellis;
+    Player player;
+    uint8_t ispressed[ydim*xdim];
+    uint32_t singlecolors[ydim*xdim];
+    uint32_t minecolor[4] = { 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF };
+};
+
+class BattleMode : public SingleMode {
+public:
+    BattleMode();
     ~BattleMode() {};
 
-    void setPlayer(uint16_t redmine, uint16_t bluemine);
+    void setGame(uint16_t redmine, uint16_t bluemine);
+    void setColorArray(Player p, uint32_t *arr);
 
     void setCallback(char turn);
-    TrellisCallback red_ON(keyEvent evt);
-    TrellisCallback blue_ON(keyEvent evt);
+    void (TrellisCallback *)red_ON(keyEvent evt);
+    void (TrellisCallback *)blue_ON(keyEvent evt);
 
-protected:
+    void startGame(void);
+
+    void showMine(Player p, uint32_t *arr);
+
+public:
+    Adafruit_NeoTrellis *_trellis;
     Player playerRed, playerBlue;
+    uint32_t redcolors[ydim * xdim];
+    uint32_t bluecolors[ydim * xdim];
 };
 
 #endif // !MINEGAME_H
