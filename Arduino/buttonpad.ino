81e5b475319487e713e911c6a198a09d8339adba
ByungSooSon <sonbs95@naver.com>
2020-06-07 00:29:57 +0900

    시작 애니메이션 수정

--- Arduino/buttonpad.ino
+++ Arduino/buttonpad.ino
@@ -50,6 +50,10 @@ TrellisCallback blue_ON(keyEvent evt);
 
 static uint8_t ispressed[Y_DIM*X_DIM]; // button state. 1 is pressed, 0 is not pressed
 
+// 애니메이션
+static int[] background = {0,1,2,9,10,11,12,13,22,23,24,35,86,93,99,104,108,112,113,114,115,119,120,121,130,131,132,133,134,141,142,143}
+static int[] sunglasses = {38,39,40,43,44,45,48,49,50,51,52,53,54,55,56,57,58,59,62,63,64,67,68,69}
+
 // 시리얼 수신 변수
 static String sig = "";
 
@@ -211,6 +215,17 @@ void showMine(uint16_t mine_key) {
 
 }
 
+// 배열 속 값 존재여부
+int isExist(int a[], int key){
+    for(int i = 0, i < sizeof(a), i++){
+        if(a[i]==key){
+            return true;
+        }
+    }
+    return false;
+}
+
+// 라즈베리파이와의 통신 함수
 void communication(string sig)
 {
     // 문자열 슬라이싱 (Mode or Mine or Turn or Dang)
@@ -233,26 +248,32 @@ void communication(string sig)
                 setPlayer(&pSingle, "Single");
                 setPlayer(&pRed, "Red");
                 setPlayer(&pBlue, "Blue");
-                
-                // starting effect
+
                 for(int i=0; i<Y_DIM*X_DIM; i++) {
-                    trellis.setPixelColor(i, Wheel(map(i, 0, X_DIM*Y_DIM, 0, 255)));
+                    // starting effect
+                    // 선글라스 부분
+                    if (isExist(sunglasses,i))
+                        trellis.setPixelColor(i, 0x0085FF);
+                    // 배경 부분
+                    else if (isExist(background,i))
+                        trellis.setPixelColor(i, 0xE3E3E3);
+                    // 이모티콘 얼굴 부분
+                    else trellis.setPixelColor(i, 0xFFFF00);
+                    
                     trellis.show();
                     delay(20);
                 }
             
                 for(int i=0; i<Y_DIM*X_DIM; i++) {
-                    // activate rising edge on all keys
-                    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_RISING, true);
-            
                     // all neopixels off
                     trellis.setPixelColor(i, 0x000000);
                     trellis.show();
-            
-                    delay(20);
+
+                    // activate rising edge on all keys
+                    trellis.activateKey(i, SEESAW_KEYPAD_EDGE_RISING, true);
+                    delay(10);
                 }                 
-            }
-             
+            }  
         }
     }
     // register a callback for all keys
